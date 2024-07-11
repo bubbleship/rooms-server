2a0fbb8458397fb8fbca20c92af7966c57838f12
bubbleship <84926115+bubbleship@users.noreply.github.com>
2024-07-11 17:28:23 +0300

    chore: init

---
+++ src/test/java/net/rooms/RoomsServer/RoomsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.rooms.RoomsServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
