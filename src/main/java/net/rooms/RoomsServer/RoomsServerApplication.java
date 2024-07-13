85fe6a4738c6e6b8e705cc9575bdb426520a3266
bubbleship <84926115+bubbleship@users.noreply.github.com>
2024-07-13 15:20:25 +0300

    chore: remove experimental code
    Remove experimental UPD code from 'main' method

--- src/main/java/net/rooms/RoomsServer/RoomsServerApplication.java
+++ src/main/java/net/rooms/RoomsServer/RoomsServerApplication.java
@@ -3,21 +3,10 @@ package net.rooms.RoomsServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-import java.net.DatagramPacket;
-
-import static net.rooms.RoomsServer.udp.Connection.receive;
-import static net.rooms.RoomsServer.udp.Connection.send;
-
 @SpringBootApplication
 public class RoomsServerApplication {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		SpringApplication.run(RoomsServerApplication.class, args);
-		DatagramPacket packet = receive();
-		System.out.println(new String(packet.getData()));
-		packet = receive();
-		System.out.println(new String(packet.getData()));
-		send("Blah", packet.getAddress(), packet.getPort());
 	}
 }
