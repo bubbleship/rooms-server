daec63db5277d13d2f2510541559f8942c9198bc
bubbleship <84926115+bubbleship@users.noreply.github.com>
2024-07-13 15:45:49 +0300

    feat(user): add user service
    Add service for signing up users

---
+++ src/main/java/net/rooms/RoomsServer/user/UserService.java
@@ -0,0 +1,44 @@
+package net.rooms.RoomsServer.user;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class UserService implements UserDetailsService {
+
+	private final static String USER_NOT_FOUND_MSG = "user with username %s not found";
+
+	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+		this.userRepository = userRepository;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return userRepository.findByUsername(username)
+				.orElseThrow(() ->
+						new UsernameNotFoundException(
+								String.format(USER_NOT_FOUND_MSG, username)));
+	}
+
+	public void signupUser(String nickname, String username, String password, UserRole role) {
+		boolean userExists = userRepository
+				.findByUsername(username)
+				.isPresent();
+
+		if (userExists) throw new IllegalStateException("username already taken");
+
+		password = bCryptPasswordEncoder.encode(password);
+		Long uid = userRepository.lastID() + 1;
+
+		userRepository.create(new User(uid, nickname, username, password, role, LocalDateTime.now()));
+	}
+}
