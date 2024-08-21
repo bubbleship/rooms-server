de0ca91a72ee048d52269c5910d4523416eb890d
bubbleship <84926115+bubbleship@users.noreply.github.com>
2024-08-21 11:41:52 +0300

    docs(room): add missing documentation to Room.java

--- src/main/java/net/rooms/RoomsServer/room/Room.java
+++ src/main/java/net/rooms/RoomsServer/room/Room.java
@@ -2,6 +2,17 @@ package net.rooms.RoomsServer.room;
 
 import java.time.LocalDateTime;
 
+/**
+ * Represent a single room the same way it is stored in the 'room' table in the database.
+ *
+ * @param roomID       A unique identifier for the room.
+ * @param title        The title/name of the room.
+ * @param isPrivate    Indicates whether the room would appear on public search queries.
+ * @param password     A key phrase required to enter the room.
+ * @param owner        The username of the user that created the room.
+ * @param creationDate The date and time when the room was created.
+ * @param description  A string of text that describes the room.
+ */
 public record Room(
 		long roomID,
 		String title,
