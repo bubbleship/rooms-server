adbd6b5f9559e0b2eedec0d05ab8efc792fd4e0c
bubbleship <84926115+bubbleship@users.noreply.github.com>
2024-08-22 20:15:58 +0300

    docs: add missing documentation to JoinRequest.java

--- src/main/java/net/rooms/RoomsServer/room/requests/JoinRequest.java
+++ src/main/java/net/rooms/RoomsServer/room/requests/JoinRequest.java
@@ -1,5 +1,12 @@
 package net.rooms.RoomsServer.room.requests;
 
+/**
+ * Represents a request to join a room. Requests for joining a room are converted to this record
+ * for easy access.
+ *
+ * @param roomID   The identifier of the room the user is attempting to join.
+ * @param password The password given by the user to join the room.
+ */
 public record JoinRequest(
 		long roomID,
 		String password
