dfbba7c1d80a175ff272b0f861bf77d0922b72d6
bubbleship <84926115+bubbleship@users.noreply.github.com>
2024-08-22 20:17:26 +0300

    docs: add missing documentation to LeaveRequest.java

--- src/main/java/net/rooms/RoomsServer/room/requests/LeaveRequest.java
+++ src/main/java/net/rooms/RoomsServer/room/requests/LeaveRequest.java
@@ -1,5 +1,11 @@
 package net.rooms.RoomsServer.room.requests;
 
+/**
+ * Represents a request to leave a room. Requests for leaving a room are converted to this record
+ * for easy access.
+ *
+ * @param roomID The room the user is attempting to leave.
+ */
 public record LeaveRequest(
 		long roomID
 ) {
