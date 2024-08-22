bfb6d15d1a5041d4b1fdcb2e45404b2fd8d5f905
bubbleship <84926115+bubbleship@users.noreply.github.com>
2024-08-22 19:34:17 +0300

    docs: add missing documentation to Participant.java

--- src/main/java/net/rooms/RoomsServer/user/Participant.java
+++ src/main/java/net/rooms/RoomsServer/user/Participant.java
@@ -2,6 +2,15 @@ package net.rooms.RoomsServer.user;
 
 import java.time.LocalDateTime;
 
+/**
+ * Represents a participant in a room. Contains only the public properties of a user. See
+ * {@link User}.
+ *
+ * @param roomID     The identifier of the room where the specified user is a participant.
+ * @param nickname   The display name of the participant.
+ * @param username   The username of the participant.
+ * @param signupDate The date when the user was created.
+ */
 public record Participant(
 		long roomID,
 		String nickname,
