e4542be8c7eaca0e95099ee59bf00604a981543b
bubbleship <84926115+bubbleship@users.noreply.github.com>
2024-08-22 20:35:07 +0300

    docs: add missing documentation to RegistrationController.java
    Add documentation to endpoint:
    - api/v1/registration

--- src/main/java/net/rooms/RoomsServer/registration/RegistrationController.java
+++ src/main/java/net/rooms/RoomsServer/registration/RegistrationController.java
@@ -16,6 +16,21 @@ public class RegistrationController {
 
 	private final RegistrationService registrationService;
 
+	/**
+	 * Accepts REST API POST requests for creating a new user account.
+	 * JSON content request example:
+	 * <code>
+	 * {
+	 *   "nickname" : "nickname",
+	 *   "username" : "username",
+	 *   "password" : "12345678",
+	 *   "role" : 0
+	 * }
+	 * </code>
+	 *
+	 * @param request The details for the new account.
+	 * @return A string with an error message in case the operation failed. Otherwise, "success".
+	 */
 	@PostMapping
 	public String register(@RequestBody RegistrationRequest request) {
 		return registrationService.register(request);
