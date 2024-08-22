8760fbce8e6c16d5da247b67bcb41d67d17856b0
bubbleship <84926115+bubbleship@users.noreply.github.com>
2024-08-22 19:08:53 +0300

    docs(message): add missing documentation to Message.java

--- src/main/java/net/rooms/RoomsServer/message/Message.java
+++ src/main/java/net/rooms/RoomsServer/message/Message.java
@@ -2,6 +2,17 @@ package net.rooms.RoomsServer.message;
 
 import java.time.LocalDateTime;
 
+/**
+ * Represents a single message in a room the same way it is stored in the 'message' table in the
+ * database.
+ *
+ * @param id       A unique identifier for the message.
+ * @param roomID   A unique identifier for the room to which the message is for.
+ * @param type     Indicates how the message properties should be interpreted.
+ * @param sender   The username of the author of this message.
+ * @param content  The content of the message.
+ * @param sendDate The date and time the message was processed by the server.
+ */
 public record Message(
 		long id,
 		long roomID, // The room to which the message is for
