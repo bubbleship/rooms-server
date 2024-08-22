e1fd6830fa7d5714f131cb135679c6c29dd6d1d8
bubbleship <84926115+bubbleship@users.noreply.github.com>
2024-08-22 19:50:12 +0300

    docs: add missing documentation to CustomSessionListener.java

--- src/main/java/net/rooms/RoomsServer/CustomSessionListener.java
+++ src/main/java/net/rooms/RoomsServer/CustomSessionListener.java
@@ -8,20 +8,29 @@ import org.springframework.stereotype.Component;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * A listener and repository of session IDs used exclusively to provide authentication for requests
+ * received over the WS connection.
+ */
 @Component
 public class CustomSessionListener implements HttpSessionListener {
 
+	/**
+	 * A {@link Map} used to associate session ID tokens with their respected sessions.
+	 */
 	private static final Map<String, HttpSession> sessions = new ConcurrentHashMap<>();
 
 	@Override
 	public void sessionCreated(HttpSessionEvent event) {
 		HttpSession session = event.getSession();
+		// Queries to this class are expected to begin with "JSESSIONID=".
 		sessions.put("JSESSIONID=" + session.getId(), session);
 	}
 
 	@Override
 	public void sessionDestroyed(HttpSessionEvent event) {
 		HttpSession session = event.getSession();
+		// Queries to this class are expected to begin with "JSESSIONID=".
 		sessions.remove(session.getId());
 	}
 
