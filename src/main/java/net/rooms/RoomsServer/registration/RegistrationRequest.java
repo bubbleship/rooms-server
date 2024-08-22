0b914d23f617139bdf0b063ac2108e0b9284326e
bubbleship <84926115+bubbleship@users.noreply.github.com>
2024-08-22 20:28:35 +0300

    docs: add missing documentation to RegistrationRequest.java

--- src/main/java/net/rooms/RoomsServer/registration/RegistrationRequest.java
+++ src/main/java/net/rooms/RoomsServer/registration/RegistrationRequest.java
@@ -3,7 +3,13 @@ package net.rooms.RoomsServer.registration;
 import net.rooms.RoomsServer.user.UserRole;
 
 /**
- * Represents a signup request.
+ * Represents a signup request. Requests for creating a new account are converted to this record
+ * for easy access.
+ *
+ * @param nickname The display name of the new user.
+ * @param username The username of the new user (used for authentication).
+ * @param password The password for the account (used for authentication).
+ * @param role     Indicates whether to create a user or an admin account,
  */
 public record RegistrationRequest(
 		String nickname,
