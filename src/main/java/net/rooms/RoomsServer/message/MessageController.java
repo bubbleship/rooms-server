d882aef97657bc0ca23442ff59d19002cd01d759
bubbleship <84926115+bubbleship@users.noreply.github.com>
2024-08-22 19:14:52 +0300

    docs(message): add missing documentation to MessageController.java
    Add documentation to websocket endpoint:
    - app/message
    Add documentation to endpoint:
    - messages/{roomID}

--- src/main/java/net/rooms/RoomsServer/message/MessageController.java
+++ src/main/java/net/rooms/RoomsServer/message/MessageController.java
@@ -23,6 +23,16 @@ public class MessageController {
 	private final SimpMessagingTemplate template;
 	private final MessageService messageService;
 
+	/**
+	 * Accepts WS requests for posting a new message in a room.
+	 * Only requests with a valid session id that is associated with a user who is a participant in
+	 * the specified room would be honored.
+	 * Sends a notification with the newly created message to all participants of the room, if the
+	 * request was successful, at "/queue/messages".
+	 *
+	 * @param json The payload containing the message and various other details. See
+	 *             {@link MessageRequest}.
+	 */
 	@MessageMapping("/message")
 	public void processMessage(@Payload String json) {
 		MessageRequest request = JSON.fromJson(json, MessageRequest.class);
@@ -36,6 +46,14 @@ public class MessageController {
 			template.convertAndSendToUser(participant.username(), "/queue/messages", parsedMessage);
 	}
 
+	/**
+	 * Accepts REST API GET requests for the list of message of a given room.
+	 * Only logged-in users who are participants in the room may receive such a list.
+	 *
+	 * @param roomID The ID of the room from which the list is requested.
+	 * @param user   The currently logged-in user.
+	 * @return A list of the messages from the specified room.
+	 */
 	@GetMapping(path = "messages/{roomID}")
 	public ResponseEntity<List<Message>> list(@PathVariable("roomID") long roomID, @AuthenticationPrincipal User user) {
 		return ResponseEntity.ok(messageService.list(roomID, user));
